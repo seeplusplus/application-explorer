45b7aff2260fb3b6544d13199015c3a8acc717f4
Caleb Webber <caleb@codingthemsoftly.com>
2021-04-17 03:23:04 -0400

    feat: add environment specific service urls

--- angular/src/app/applications.service.ts
+++ angular/src/app/applications.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { Application } from './application';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicationsService {
   //private _getApplicationsUrl = "http://localhost:7071/api/applications";
-  private _getApplicationsUrl = "assets/data.json";
+  private _getApplicationsUrl = environment.dataUrl;
 
   constructor(private httpClient: HttpClient) { }
 
