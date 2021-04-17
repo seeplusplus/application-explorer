c1760d70e3251afcb54054c45e6dbfffaa9b7dc6
Caleb Webber <caleb@codingthemsoftly.com>
2021-04-17 01:42:29 -0400

    feat: add tooltips and redirect wildcard route

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { ApplicationHomeComponent } from './application-home/application-home.co
 import { ApplicationDetailComponent } from './application-detail/application-detail.component'
 
 const routes: Routes = [
+  { path: 'detail/:id', component: ApplicationDetailComponent },
   { path: '', component: ApplicationHomeComponent },
-  { path: 'detail', component: ApplicationDetailComponent }
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -24,6 +24,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip'; 
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { DayNamePipe } from './day-name.pipe'; 
@@ -60,6 +61,7 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatTooltipModule,
     MatButtonToggleModule,
     ReactiveFormsModule
   ],
